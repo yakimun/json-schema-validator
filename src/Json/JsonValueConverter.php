8d2ee659632b3050c768541093d0ba692be58d96
Sergey Yakimov <yakimun@gmail.com>
2021-03-27 15:30:35 +0200

    Improve type checks in converter to JSON value wrappers

--- src/Json/JsonValueConverter.php
+++ src/Json/JsonValueConverter.php
@@ -4,13 +4,15 @@ declare(strict_types=1);
 
 namespace Yakimun\JsonSchemaValidator\Json;
 
+use Yakimun\JsonSchemaValidator\Exception\InvalidValueException;
+
 /**
  * @psalm-immutable
  */
 final class JsonValueConverter
 {
     /**
-     * @param null|scalar|object|list<mixed> $value
+     * @param mixed $value
      * @return JsonValue
      */
     public function convert($value): JsonValue
@@ -19,7 +21,7 @@ final class JsonValueConverter
     }
 
     /**
-     * @param null|scalar|object|list<mixed> $value
+     * @param mixed $value
      * @param JsonPointer $path
      * @return JsonValue
      */
@@ -49,7 +51,12 @@ final class JsonValueConverter
             return new JsonFloat($value, $path);
         }
 
-        return new JsonString($value, $path);
+        if (is_string($value)) {
+            return new JsonString($value, $path);
+        }
+
+        $format = 'The value must be a null, a boolean, an object, an array, an int, a float, or a string. Path: "%s".';
+        throw new InvalidValueException(sprintf($format, (string)$path));
     }
 
     /**
@@ -63,7 +70,7 @@ final class JsonValueConverter
 
         /**
          * @var string $key
-         * @var null|scalar|object|list<mixed> $property
+         * @var null|scalar|object|array<array-key, mixed> $property
          */
         foreach ($value as $key => $property) {
             $properties[$key] = $this->convertValue($property, $path->addToken($key));
@@ -73,19 +80,26 @@ final class JsonValueConverter
     }
 
     /**
-     * @param list<mixed> $value
+     * @param array<array-key, mixed> $value
      * @param JsonPointer $path
      * @return JsonArray
      */
     private function convertArrayValue(array $value, JsonPointer $path): JsonArray
     {
         $items = [];
+        $expectedIndex = 0;
 
         /**
-         * @var null|scalar|object|list<mixed> $item
+         * @var null|scalar|object|array<array-key, mixed> $item
          */
         foreach ($value as $index => $item) {
+            if ($index !== $expectedIndex) {
+                $format = 'The array keys must be integers starting from 0 with no gaps in between. Path: "%s".';
+                throw new InvalidValueException(sprintf($format, (string)$path));
+            }
+
             $items[] = $this->convertValue($item, $path->addToken((string)$index));
+            $expectedIndex++;
         }
 
         return new JsonArray($items, $path);
--- tests/Json/JsonValueConverterTest.php
+++ tests/Json/JsonValueConverterTest.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Yakimun\JsonSchemaValidator\Tests\Json;
 
 use PHPUnit\Framework\TestCase;
+use Yakimun\JsonSchemaValidator\Exception\InvalidValueException;
 use Yakimun\JsonSchemaValidator\Json\JsonArray;
 use Yakimun\JsonSchemaValidator\Json\JsonFalse;
 use Yakimun\JsonSchemaValidator\Json\JsonFloat;
@@ -14,7 +15,6 @@ use Yakimun\JsonSchemaValidator\Json\JsonObject;
 use Yakimun\JsonSchemaValidator\Json\JsonPointer;
 use Yakimun\JsonSchemaValidator\Json\JsonString;
 use Yakimun\JsonSchemaValidator\Json\JsonTrue;
-use Yakimun\JsonSchemaValidator\Json\JsonValue;
 use Yakimun\JsonSchemaValidator\Json\JsonValueConverter;
 
 /**
@@ -32,45 +32,135 @@ use Yakimun\JsonSchemaValidator\Json\JsonValueConverter;
 final class JsonValueConverterTest extends TestCase
 {
     /**
-     * @param null|scalar|object|list<mixed> $value
-     * @param JsonValue $expected
-     *
-     * @dataProvider valueProvider
+     * @var JsonValueConverter
      */
-    public function testConvert($value, JsonValue $expected): void
-    {
-        $jsonValueConverter = new JsonValueConverter();
+    private $jsonValueConverter;
 
-        $this->assertEquals($expected, $jsonValueConverter->convert($value));
+    protected function setUp(): void
+    {
+        $this->jsonValueConverter = new JsonValueConverter();
+    }
+
+    public function testConvertNull(): void
+    {
+        $jsonValue = $this->jsonValueConverter->convert(null);
+        $this->assertEquals(new JsonNull(new JsonPointer()), $jsonValue);
+    }
+
+    public function testConvertTrue(): void
+    {
+        $jsonValue = $this->jsonValueConverter->convert(true);
+        $this->assertEquals(new JsonTrue(new JsonPointer()), $jsonValue);
+    }
+
+    public function testConvertFalse(): void
+    {
+        $jsonValue = $this->jsonValueConverter->convert(false);
+        $this->assertEquals(new JsonFalse(new JsonPointer()), $jsonValue);
     }
 
     /**
-     * @return list<array{null|scalar|object|list<mixed>, JsonValue}>
+     * @param array<string, null|true> $valueProperties
+     * @param array<string, JsonNull|JsonTrue> $expectedProperties
+     *
+     * @dataProvider objectProvider
      */
-    public function valueProvider(): array
+    public function testConvertObject(array $valueProperties, array $expectedProperties): void
     {
-        $path = new JsonPointer();
+        $jsonValue = $this->jsonValueConverter->convert((object)$valueProperties);
+        $this->assertEquals(new JsonObject($expectedProperties, new JsonPointer()), $jsonValue);
+    }
+
+    /**
+     * @return list<array{array<string, null|true>, array<string, JsonNull|JsonTrue>}>
+     */
+    public function objectProvider(): array
+    {
+        $jsonPointer = new JsonPointer();
+
+        $jsonNull = new JsonNull($jsonPointer->addToken('a'));
+        $jsonTrue = new JsonTrue($jsonPointer->addToken('b'));
 
         return [
-            [null, new JsonNull($path)],
-            [true, new JsonTrue($path)],
-            [false, new JsonFalse($path)],
-            [(object)['a' => null], new JsonObject(['a' => new JsonNull($path->addToken('a'))], $path)],
-            [
-                (object)['a' => null, 'b' => true],
-                new JsonObject(
-                    ['a' => new JsonNull($path->addToken('a')), 'b' => new JsonTrue($path->addToken('b'))],
-                    $path,
-                ),
-            ],
-            [[null], new JsonArray([new JsonNull($path->addToken('0'))], $path)],
-            [
-                [null, true],
-                new JsonArray([new JsonNull($path->addToken('0')), new JsonTrue($path->addToken('1'))], $path),
-            ],
-            [1, new JsonInteger(1, $path)],
-            [1.5, new JsonFloat(1.5, $path)],
-            ['foo', new JsonString('foo', $path)],
+            [[], []],
+            [['a' => null], ['a' => $jsonNull]],
+            [['a' => null, 'b' => true], ['a' => $jsonNull, 'b' => $jsonTrue]],
         ];
+    }
+
+    /**
+     * @param list<null|true> $value
+     * @param list<JsonNull|JsonTrue> $expected
+     *
+     * @dataProvider arrayProvider
+     */
+    public function testConvertArray(array $value, array $expected): void
+    {
+        $jsonValue = $this->jsonValueConverter->convert($value);
+        $this->assertEquals(new JsonArray($expected, new JsonPointer()), $jsonValue);
+    }
+
+    /**
+     * @return list<array{list<null|true>, list<JsonNull|JsonTrue>}>
+     */
+    public function arrayProvider(): array
+    {
+        $jsonPointer = new JsonPointer();
+
+        $jsonNull = new JsonNull($jsonPointer->addToken('0'));
+        $jsonTrue = new JsonTrue($jsonPointer->addToken('1'));
+
+        return [
+            [[], []],
+            [[null], [$jsonNull]],
+            [[null, true], [$jsonNull, $jsonTrue]],
+        ];
+    }
+
+    /**
+     * @param array<array-key, null> $value
+     *
+     * @dataProvider arrayWithInvalidKeysProvider
+     */
+    public function testConvertArrayWithInvalidKeys(array $value): void
+    {
+        $this->expectException(InvalidValueException::class);
+        $this->jsonValueConverter->convert($value);
+    }
+
+    /**
+     * @return list<list<array<array-key, null>>>
+     */
+    public function arrayWithInvalidKeysProvider(): array
+    {
+        return [
+            [['a' => null]],
+            [[1 => null]],
+            [[0 => null, 2 => null]],
+        ];
+    }
+
+    public function testConvertInt(): void
+    {
+        $jsonValue = $this->jsonValueConverter->convert(1);
+        $this->assertEquals(new JsonInteger(1, new JsonPointer()), $jsonValue);
+    }
+
+    public function testConvertFloat(): void
+    {
+        $jsonValue = $this->jsonValueConverter->convert(1.0);
+        $this->assertEquals(new JsonFloat(1.0, new JsonPointer()), $jsonValue);
+    }
+
+    public function testConvertString(): void
+    {
+        $jsonValue = $this->jsonValueConverter->convert('foo');
+        $this->assertEquals(new JsonString('foo', new JsonPointer()), $jsonValue);
+    }
+
+    public function testConvertInvalidValue(): void
+    {
+        $this->expectException(InvalidValueException::class);
+        $this->jsonValueConverter->convert(fopen('php://memory', 'rb'));
     }
 }
